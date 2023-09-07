b1392404e4af6a52a8429fe5a32313c176f14c0f
meenabva <68288484+meenabva@users.noreply.github.com>
2023-09-07 20:38:16 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import Weather from './components/Weather';
 
-function App() {
+export default function App() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  /* const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+  } */
+
+  var options = {
+    enableHighAcuracy: false,
+    timeout: 5000,
+    maximumAge: Infinity
+  };
+
+  const fetchData = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    }, (error) => {
+      console.log(error);
+    }, options);
+  
+  }
+
+  useEffect(() => {
+   fetchData()
+  }, [])
+  
+  useCallback(async () => {
+/*     if(lat==' ' && long==' ') {
+      await fetch(`${process.env.REACT_APP_API_URL}/weather?q=Bengaluru,India&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    } else { */
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(result => {
+          setData(result)
+          console.log(result);
+        });
+   
+  }, [lat, long])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ): (
+        <div>undefined</div>
+      )}
     </div>
   );
 }
-
-export default App;
