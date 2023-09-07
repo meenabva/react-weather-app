b1a9472f141e49ce39b15eb37d68b5b615ebf37a
meenabva <68288484+meenabva@users.noreply.github.com>
2023-09-07 23:17:16 +0530

    display temperature and desc

--- src/App.js
+++ src/App.js
@@ -1,65 +1,53 @@
 import './App.css';
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Weather from './components/Weather';
 
 export default function App() {
   
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [lat, setLat] = useState(12.9762);
+  const [long, setLong] = useState(77.6033);
   const [data, setData] = useState([]);
 
-  /* const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
-  } */
-
-  var options = {
+  let options = {
     enableHighAcuracy: false,
     timeout: 5000,
     maximumAge: Infinity
   };
 
-  const fetchData = () => {
+  const getLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
+      setLat(position.coords.latitude)
+      setLong(position.coords.longitude)
     }, (error) => {
-      console.log(error);
-    }, options);
-  
+      console.log(error)
+    }, options)
+    fetchData()
+  }
+
+  const fetchData = async () => {
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    }).catch((error) => {
+      console.log(error)
+    });
   }
 
   useEffect(() => {
-   fetchData()
-  }, [])
-  
-  useCallback(async () => {
-/*     if(lat==' ' && long==' ') {
-      await fetch(`${process.env.REACT_APP_API_URL}/weather?q=Bengaluru,India&appid=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    } else { */
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setData(result)
-          console.log(result);
-        });
-   
+    getLocation()
   }, [lat, long])
 
   return (
     <div className="App">
-      {(typeof data.main != 'undefined') ? (
-        <Weather weatherData={data}/>
-      ): (
-        <div>undefined</div>
-      )}
+      <div className='App-header'>
+        {(typeof data.main != 'undefined') ? (
+          <Weather weatherData={data}/>
+        ): (
+          <div>undefined</div>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 //import './styles.css';
 import { Card } from 'semantic-ui-react'
 
-const Weather = ({weatherData}) => (
-  <Card>
-    <Card.Content>
-        <Card.Header className="header">{weatherData.name}</Card.Header>
-    </Card.Content>
-  </Card>
-)
+const Weather = ({weatherData}) => {
+  
+  useEffect(() => {
+    console.log("props", weatherData)
+  }, [])
+
+  return(
+    <Card>
+      <Card.Content>
+          <Card.Header className="header">{weatherData.name}</Card.Header>
+          <Card.Content>
+          <p>{weatherData.main.temp}</p>
+          <p>{weatherData.weather[0].description}</p>
+          </Card.Content>
+      </Card.Content>
+    </Card>
+  );
+}
 
 export default Weather;
